5c2395b84c9454ada17fae5f6cdb7f2bf43292f9
Shree balaji <shreebalajisiva19@gmail.com>
2023-08-17 10:58:42 +0530

    Updated the styling of FlamesComponents

--- src/FlamesComponents/FlamesHome.jsx
+++ src/FlamesComponents/FlamesHome.jsx
@@ -1,8 +1,7 @@
 import React, { useState, createContext } from 'react'
 import "./Flames.css"
 import InputForm from './InputForm';
-import Lottie from "lottie-react"
-import balloons from "../assets/balloons.json"
+
 
 export const AppContext = createContext();
 
@@ -12,16 +11,9 @@ export default function FlamesHome() {
 
   return (
     <div id='flamesHome'>
-        <div id='flames-container'>
-            <div id='container-left'>
-              <AppContext.Provider value={{userName,setUserName,crushName,setCrushName}}>
-                  <InputForm/>
-              </AppContext.Provider>
-            </div>
-            <div id='container-right'>
-              <Lottie animationData={balloons} loop={false} />
-            </div>
-        </div>
+            <AppContext.Provider value={{userName,setUserName,crushName,setCrushName}}>
+                 <InputForm/>
+            </AppContext.Provider>
     </div>
   )
 }
--- src/FlamesComponents/InputForm.jsx
+++ src/FlamesComponents/InputForm.jsx
@@ -1,9 +1,15 @@
-import React , {useContext}from 'react'
+import React , {useState,useContext}from 'react'
 import { useFormik } from 'formik'
 import * as Yup from "yup"
 import { AppContext } from './FlamesHome'
+import {motion} from "framer-motion"
+import Lottie from "lottie-react"
+import balloons from "../assets/balloons.json"
 
 export default function InputForm() {
+
+    const[result,setResult] = useState("");
+
     const flame =(userName,crushName) =>{
         let a= userName.split("");
         let b= crushName.split(""); 
@@ -47,11 +53,14 @@ export default function InputForm() {
         },
         onSubmit: async (values) => {
             try {
+                values.firstPerson = values.firstPerson.replace(/\s/g, '');
+                values.secondPerson = values.secondPerson.replace(/\s/g, '');
+
                 setUserName(values.firstPerson);
                 setCrushName(values.secondPerson);
                 if (values.firstPerson !== "") {
-                    const result = await flame(values.firstPerson, values.secondPerson);
-                    console.log(result);
+                    const res = await flame(values.firstPerson, values.secondPerson);
+                    setResult(res);
                 }
             } catch (error) {
                 console.error("An error occurred:", error);
@@ -59,46 +68,67 @@ export default function InputForm() {
         },
         validationSchema : Yup.object({
             firstPerson:Yup.string()
-            .matches(/^[A-Za-z]+$/, 'Field can only contain alphabetic characters')
-            .max(25,"must be less than 15 characters!")
-            .min(2,"must be more than 2 characters")
+            .matches(/^[A-Za-z ]+$/, 'only alphabets')
+            .max(25,"must be < 15 characters!")
+            .min(2,"must be > 2 characters!")
             .required("Required"),
 
             secondPerson:Yup.string()
-            .matches(/^[A-Za-z]+$/, 'Field can only contain alphabetic characters')
-            .max(25,"must be less than 15 characters!")
-            .min(2,"must be more than 2 characters")
+            .matches(/^[A-Za-z ]+$/, 'only alphabets')
+            .max(25,"must be < 15 characters!")
+            .min(2,"must be > 2 characters!")
             .required("Required"),
-            
         })
     })
   return (
     <div id='inputForm'>
         <form onSubmit={formik.handleSubmit}>
-            <input name = "firstPerson"
-                type="text"
-                placeholder='Enter your name'
-                onChange={formik.handleChange}
-                value={formik.values.firstPerson} />
-            {formik.errors.firstPerson &&
-                        (formik.touched.firstPerson && 
-                            <span style={{color:"red"}}>
-                                {formik.errors.firstPerson}</span>)}
-            
-            <input name = "secondPerson"
-                type="text"
-                placeholder='Enter your name'
-                onChange={formik.handleChange}
-                value={formik.values.secondPerson} />
-            {formik.errors.secondPerson && 
-                        (formik.touched.secondPerson && 
-                            <span style={{color:"red"}}>
-                                {formik.errors.secondPerson}</span>)}
-            
-            <button className='sub-btn' type='submit' onClick={formik.onSubmit}>
-                Submit
-            </button>
+            <div id='form'>
+            <div id="If-top">
+                <div id='ip'>
+                    <div id='ip-1'>
+                        <input name = "firstPerson"
+                            className='inputs'
+                            type="text"
+                            placeholder='Enter your name'
+                            onChange={formik.handleChange}
+                            value={formik.values.firstPerson} />
 
+                        {formik.errors.firstPerson &&
+                                    (formik.touched.firstPerson && 
+                                        <span style={{color:"red"}}>
+                                            {formik.errors.firstPerson}</span>)}
+                    </div>
+                    <div id='ip-2'>
+                        <input name = "secondPerson"
+                            className='inputs'
+                            type="text"
+                            placeholder='Enter your name'
+                            onChange={formik.handleChange}
+                            value={formik.values.secondPerson} />
+                        {formik.errors.secondPerson && 
+                                    (formik.touched.secondPerson && 
+                                        <span style={{color:"red"}}>
+                                            {formik.errors.secondPerson}</span>)}
+                    </div>
+                </div>
+                <div id='sub-btn'>
+                    <button className='btn' type='submit' onClick={formik.onSubmit}>
+                        Spin it
+                    </button>
+                </div>
+            </div>
+            <div id="If-bottom">
+                <div id='display-result'>
+                    <h1 id='result' style={{color:"red",fontFamily:"cursive"}}>
+                        {result}
+                    </h1>
+                </div>
+                <div id='display-text'>
+                        <Lottie id='animation' animationData={balloons} loop={false}/>
+                </div>
+            </div>
+            </div>
         </form>
     </div>
   )
